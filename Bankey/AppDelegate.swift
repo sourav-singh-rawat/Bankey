03d44b2931bf1f2e6035e602818dd16a3cc2256f
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-12-05 07:41:16 +0530

    Implemented protocol for transition from login to onboarding

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -13,12 +13,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    let loginViewController = LoginViewController()
+    
+    let onboardingContainerViewController = OnboardingContainerViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = LoginViewController()
+        window?.rootViewController = loginViewController
+        loginViewController.delegate = self
         
         return true
     }
@@ -26,3 +31,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+//MARK: - LoginViewControllerDelegate
+
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogin() {
+        window?.rootViewController = onboardingContainerViewController
+        onboardingContainerViewController.delegate = self
+    }
+}
+
+//MARK: - OnboardingContainerViewControllerDelegate
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        print("finish onboarding")
+    }
+}
+
+extension AppDelegate {
+    //Transition
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(
+            with: window,
+            duration: 0.3,
+            options: .transitionCrossDissolve,
+            animations: nil,
+            completion: nil
+        )
+    }
+}
