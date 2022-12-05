05a63e12dfddfa54f75429dae62328618dc652fb
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-12-05 07:46:08 +0530

    Minor fix in smooth transition

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -35,8 +35,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        window?.rootViewController = onboardingContainerViewController
         onboardingContainerViewController.delegate = self
+        setRootViewController(onboardingContainerViewController)
     }
 }
 
